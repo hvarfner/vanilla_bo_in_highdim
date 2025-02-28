061ecf5a64e90ae6a01e5e20d514111a85d7bb25
hvarfner <carl.hvarfner@live.se>
2025-02-28 09:19:02 +0100

    Submit scripts

--- single_job.sh
+++ single_job.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=vanillabo         # Job name
-#SBATCH --output=logs/slurm_output_%j.log          # Output file (with job id)
-#SBATCH --error=logs/slurm_error_%j.log            # Error file (with job id)
+#SBATCH --output=logs/%j.log          # Output file (with job id)
+#SBATCH --error=logs/%j.log            # Error file (with job id)
 #SBATCH --time=4:00:00
 #SBATCH --gpus-per-node=T4:2
 #SBATCH -A NAISS2024-22-1613
-# Load Singularity module if it's not loaded by default
-module load singularity
 
 # Path to your Singularity container image (replace with the actual path)
 SINGULARITY_IMAGE="vanillabo.sif"
 
 # Command to run inside the Singularity container
 # Replace 'your_command' with the command you want to execute inside the container
-singularity exec $SINGULARITY_IMAGE $*
+singularity run $SINGULARITY_IMAGE $*
